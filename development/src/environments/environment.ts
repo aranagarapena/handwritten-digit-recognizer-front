0dae25e3478fdee9b9e16cf2d9451b787be1a1e8
Arana Jauna <arana.github@gmail.com>
2025-04-09 07:39:37 +0200

    añadidas rutas bien puestas del servidor

--- development/src/environments/environment.ts
+++ development/src/environments/environment.ts
@@ -5,26 +5,25 @@ export const environment = {
     // URL_BACKEND_IP:'http://127.0.0.1/lrvl-ng-xampp/back/public',
     // URL_FRONTEND: 'http://localhost:4200',
 
-    // URL_PRUEBA_POST:'https://jsonplaceholder.typicode.com/post', // TODO: nocarga bien estas variables de entorno
+    // URL_PRUEBA_POST:'https://jsonplaceholder.typicode.com/post', // TODO: no carga bien estas variables de entorno
     
     // URL_API_USER_REGISTER:'/api/usuarios/signin', // dar de alta un nuevo usuario
     // URL_API_USER_GET_ALL:'/api/usuarios/getusers', // resuperar todos los usuarios de la BDs
     // URL_API_NUMBER_ADD:'/api/game/storenumber'
 
-    // new DNS: handwrite-digit-recognition.com
+    // new DNS: handwritten-digit-recognition.com
 
     production: false,
 
-    URL_BACKEND_DNS:'http://api-laravel.example.com',
-    URL_BACKEND: 'http://localhost/lrvl-ng-xampp/back/public',
-    URL_BACKEND_IP_PORT:'http://127.0.0.1:80/lrvl-ng-xampp/back/public',
-    URL_BACKEND_IP:'http://127.0.0.1/lrvl-ng-xampp/back/public',
+    URL_BACKEND_DNS:'http://handwritten-digit-recognition.com',
+    URL_BACKEND: 'http://localhost/handwritten-digit-recognizer/back/development/public',
+    URL_BACKEND_IP_PORT:'http://127.0.0.1:80/handwritten-digit-recognizer/back/development/public',
+    URL_BACKEND_IP:'http://127.0.0.1/handwritten-digit-recognizer/back/development/public',
 
     URL_FRONTEND: 'http://localhost:4200',
 
     URL_API_USER_REGISTER: '/registro',
     URL_API_USER_GET_ALL: '/getusers',
     URL_API_NUMBER_ADD: '/api/game/storenumber'
-
 };
 
